7509ddc95f53427bb43f7e14928c433baa7c255d
Rostik Krysko <rostik15@hotmail.com>
2018-02-13 21:14:23 -0600

    Created main object structure

---
+++ CustomerEnvironmentViewer/CustomerEnvironmentViewer/Model/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerEnvironmentViewer.Model
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+
+        public string CustomerDirectory { get; set; }
+
+        public List<Environment> EnvironmentCollection { get; set; }
+    }
+}
---
+++ CustomerEnvironmentViewer/CustomerEnvironmentViewer/Model/Environment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerEnvironmentViewer.Model
+{
+    public class Environment : Customer
+    {
+        public string EnvironmentName { get; set; }
+
+        public string EnvironmentDirectory { get; set; }
+
+        public EnvironmentDetails Details { get; set; }
+    }
+}
